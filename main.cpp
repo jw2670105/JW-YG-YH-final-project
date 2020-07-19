90c1addd48729acd9bd997d5e1195cd0a0e34adf
yanjun934523 <yguan030@ucr.edu>
2020-07-19 16:12:07 -0700

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "base.hpp"
+#include "calculator.hpp"
+#include "calculator2.hpp"
+#include "decorator.hpp"
+#include "result.hpp"
+#include "num.hpp"
+
+int main()
+{
+
+	return 0;
+
+}
+
