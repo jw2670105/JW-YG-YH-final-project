213b2750a85300c5238a6a509c4ace52c8f4e38c
yihuaH <haoyihua88@gmail.com>
2020-07-21 00:46:45 -0700

    merge two calculator.hpp to one

--- calculator.hpp
+++ calculator.hpp
@@ -12,20 +12,32 @@ using namespace std;
 class Calculator : public Base
 {
 private:
-	Base* kg;
-	Base* meter;
+	Base* function_choice
+	Base* weight;
+	Base* height;
+	
 public:
-	Calculator(Base* weight, Base* height) :Base()
+	Calculator(vector<Base*> input) :Base()
 	{
-		kg = weight;
-		meter = height;
+		function_choice = input.at(0);
+		weight = input.at(1);
+		height = input.at(2);
 	}
 	virtual double evaluate()
 	{	
-		Op* two = new Op(2);
-		Pow* result = new Pow(meter, two);
-		Div* result2 = new Div(kg,result);
-		return result2->evaluate();
+		if(function_choice->evaluate() == 1){
+			Op* two = new Op(2);
+			Pow* result = new Pow(height, two);
+			Div* result2 = new Div(weight,result);
+			return result2->evaluate();
+		}else if (function_choice->evaluate() == 2){
+			Op* two = new Op(2);
+			Op* sot = new Op(703);
+			Mult* result = new Mult(sot, weight);
+			Pow* result2 = new Pow(height,two);
+			Div* result3 = new Div(result, result2);
+			return result3->evaluate();		
+		}
 	}
 };
 #endif
--- factory.hpp
+++ factory.hpp
@@ -10,7 +10,7 @@ class Factory {
 private:
 	vector<string> string_v;
 	vector<Base*> result;
-	Base* function_chice;
+	Base* function_choice;
 	Base* weight;
 	Base* height;
 public:
@@ -22,35 +22,14 @@ public:
 		}
 
 		vector<Base*> string_to_base(){
-			left = new Op(stoi(string_v.at(0)));
-			result.push_back(left);
-			for(int i = 1; i < string_v.size()-1; i++){
-				if((i + 1) % 2 == 0){
-					right = new Op(stoi(string_v.at(i + 1)));
-				}
-					left = result.back();
-					if(string_v.at(i) == "+"){
-						base = new Add(left, right);
-						result.push_back(base);
-					} else if (string_v.at(i) == "-"){
-						base = new Sub(left, right);
-						result.push_back(base);
-					} else if (string_v.at(i) == "*"){
-						base = new Mult(left, right);
-						result.push_back(base);
-					} else if (string_v.at(i) == "/"){
-						base = new Div(left, right);
-						result.push_back(base);
-					} else if (string_v.at(i) == "**"){
-						base = new Pow(left, right);
-						result.push_back(base);
-					}
-				}
-			
+			function_choice = new Op(stoi(string_v.at(0)));
+			result.push_back(function_choice);
+			weight = new Op(stoi(string_v.at(1)));
+			result.push_back(weight);
+			height = new Op(stoi(string_v.at(2)));
+			result.push_back(height);			
 			return result;
-
 		}
-
 };
 
 #endif
