0efb17b2c4715b53254312c3126b5c0bf61a17b4
yihuaH <haoyihua88@gmail.com>
2020-07-21 02:16:57 -0700

    add factory test

--- factory_test.hpp
+++ factory_test.hpp
@@ -10,20 +10,72 @@
 #include "factory.hpp"
 #include "calculator.hpp"
 
-TEST(Factory_test, subTest)
+TEST(Factory_test, Metric_Test1)
 {
     string test_val;
     vector<string>test_vector;
     test_val = "1";
     test_vector.push_back(test_val);
-    test_val = "100";//weight
+    test_val = "70";//weight
     test_vector.push_back(test_val);
-    test_val = "100";//height
+    test_val = "1.8";//height
     test_vector.push_back(test_val);
     Factory* factory = new Factory(test_vector);
 
     vector<Base*> test_result = factory->string_to_base();
+    Calculator* result = new Calculator(test_result);
     
+    EXPECT_NEAR(result->evaluate(), 21.6049, 0.01);   
+}
+TEST(Factory_test, Metric_Test2)
+{
+    string test_val;
+    vector<string>test_vector;
+    test_val = "1";
+    test_vector.push_back(test_val);
+    test_val = "80";//weight
+    test_vector.push_back(test_val);
+    test_val = "1.6";//height
+    test_vector.push_back(test_val);
+    Factory* factory = new Factory(test_vector);
+
+    vector<Base*> test_result = factory->string_to_base();
+    Calculator* result = new Calculator(test_result);
+
+    EXPECT_NEAR(result->evaluate(), 31.25, 0.01);
 }
 
+TEST(Factory_test, Standard_Test1)
+{
+    string test_val;
+    vector<string>test_vector;
+    test_val = "2";
+    test_vector.push_back(test_val);
+    test_val = "140";//weight
+    test_vector.push_back(test_val);
+    test_val = "71";//height
+    test_vector.push_back(test_val);
+    Factory* factory = new Factory(test_vector);
+
+    vector<Base*> test_result = factory->string_to_base();
+    Calculator* result = new Calculator(test_result);
+    EXPECT_NEAR(result->evaluate(), 19.5239, 0.01);
+}
+
+TEST(Factory_test, Standard_Test2)
+{
+    string test_val;
+    vector<string>test_vector;
+    test_val = "2";
+    test_vector.push_back(test_val);
+    test_val = "160";//weight
+    test_vector.push_back(test_val);
+    test_val = "60";//height
+    test_vector.push_back(test_val);
+    Factory* factory = new Factory(test_vector);
+
+    vector<Base*> test_result = factory->string_to_base();
+    Calculator* result = new Calculator(test_result);
+    EXPECT_NEAR(result->evaluate(), 31.2444, 0.01);
+}
 #endif //__OP_TEST_HPP__
